e37ff1117b223d551f70b3b5e209092f70a7d37f
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-06-17 17:36:34 +0300

    CREATION OF DBSETES

--- DoctorWhoCoreDbContext/DoctorWhoCoreDbContext.cs
+++ DoctorWhoCoreDbContext/DoctorWhoCoreDbContext.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DoctorWho.Db.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoctorWho.Db
 {
     public class DoctorWhoCoreDbContext : DbContext
     {
+        DbSet<Enemy> Enemies { get; set; }
+        DbSet <Author> Authors { get; set; }
+        DbSet<Episode> Episodes { get; set; }
+        DbSet<Doctor> Doctors { get; set; }
+        DbSet<Companion> Companions { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
