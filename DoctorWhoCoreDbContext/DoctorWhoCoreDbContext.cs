ed4cc57eb759be61609f7b29f775f840feea9594
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-06-17 17:31:51 +0300

    Create DBCONTEXT

---
+++ DoctorWhoCoreDbContext/DoctorWhoCoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DoctorWho.Db
+{
+    public class DoctorWhoCoreDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("ConnectionString");
+        }
+
+
+    }
+}
